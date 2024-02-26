d9f93db063643de14b876caad80f829a41f8f707
jhLee <kkndw2659@kakao.com>
2024-02-26 16:21:22 +0900

    더블탭 비활성화시 링크연결 문제로 원복

--- layouts/pageLayout.vue
+++ layouts/pageLayout.vue
@@ -11,41 +11,6 @@ export default {
   name: 'SubLayout',
   components: {
     MainFooter
-  },
-  data() {
-    return {
-      touchstartX: 0,
-      touchendX: 0
-    }
-  },
-  mounted() {
-    this.setupDoubleTapZoom()
-  },
-  methods: {
-    setupDoubleTapZoom() {
-      const handleTouchStart = (event) => {
-        this.touchstartX = event.changedTouches[0].screenX
-      }
-
-      const handleTouchEnd = (event) => {
-        this.touchendX = event.changedTouches[0].screenX
-        this.handleGesture(event)
-      }
-
-      this.$el.addEventListener('touchstart', handleTouchStart, false)
-      this.$el.addEventListener('touchend', handleTouchEnd, false)
-    },
-    handleGesture(event) {
-      if (event && event.type === 'touchend' && event.cancelable) {
-        if (Math.abs(this.touchendX - this.touchstartX) < 10) {
-          // This is a tap, not a swipe or pinch
-          // Handle your tap logic here
-
-          // For example, prevent default behavior for double tap
-          event.preventDefault()
-        }
-      }
-    }
   }
 }
 </script>
